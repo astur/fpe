1f815f75b767128afb6822b42dcafd31b7802208
astur <astur@yandex.ru>
2018-01-21 15:41:57 +0300

    type conversion stub

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const {validKeys, flatten} = require('ofl');
 
+const conv = (value, type) => value;
+
 const fpe = (options, raw) => {
     const result = {};
     validKeys(options.map(v => v.name));
     options.forEach((v, i, a) => {
         if(v.key in process.env){
-            result[v.name] = process.env[v.key];
+            result[v.name] = conv(process.env[v.key], v.type);
         }
     });
     return raw ? result : flatten(result);
