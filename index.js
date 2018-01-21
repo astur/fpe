c127e9d1f0c4db72abe80d14c8e51a4b7e939e71
astur <astur@yandex.ru>
2018-01-21 15:40:40 +0300

    base env vars extraction

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+const {validKeys, flatten} = require('ofl');
+
 const fpe = (options, raw) => {
-    //
+    const result = {};
+    validKeys(options.map(v => v.name));
+    options.forEach((v, i, a) => {
+        if(v.key in process.env){
+            result[v.name] = process.env[v.key];
+        }
+    });
+    return raw ? result : flatten(result);
 };
 
 module.exports = options => fpe(options, false);
